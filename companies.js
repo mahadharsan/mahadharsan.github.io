890679ffefb25b6596dce264bf72219321553a55
mahadharsan <maha.dharsan1999@gmail.com>
2026-03-18 16:30:42 -0400

    Update companies.js and learn.html with new content
    - Changed the company name from "Amazon" to "Amazon New Grad" for clarity.
    - Replaced the placeholder "Coming Soon" section in learn.html with a new entry for "Pareto Analysis," including a description and a link for further learning.

--- companies.js
+++ companies.js
@@ -27,7 +27,7 @@ export const companiesData = [
         url: "https://www.metacareers.com/jobsearch?sort_by_new=true&offices[0]=Ashburn%2C%20VA&offices[1]=Boston%2C%20MA&offices[2]=Austin%2C%20TX&offices[3]=Atlanta%2C%20GA&offices[4]=Garland%2C%20TX&offices[5]=Stanton%20Springs%2C%20GA&offices[6]=Newton%20County%2C%20GA&offices[7]=Gallatin%2C%20TN&offices[8]=Bowling%20Green%2C%20OH&offices[9]=Mesa%2C%20AZ&offices[10]=Aiken%2C%20SC&offices[11]=Aurora%2C%20IL&offices[12]=Altoona%2C%20IA&offices[13]=Chandler%2C%20AZ&offices[14]=North%20America&offices[15]=Los%20Angeles%2C%20CA&offices[16]=Huntsville%2C%20AL&offices[17]=Montgomery%2C%20AL&offices[18]=New%20Albany%2C%20OH&offices[19]=Seattle%2C%20WA&offices[20]=Sandston%2C%20VA&offices[21]=Sterling%2C%20VA&offices[22]=San%20Diego%2C%20CA&offices[23]=Sausalito%2C%20CA&offices[24]=Salt%20Lake%2C%20UT&offices[25]=Sunnyvale%2C%20CA&offices[26]=Santa%20Clara%2C%20CA&offices[27]=San%20Mateo%2C%20CA&offices[28]=San%20Francisco%2C%20CA&offices[29]=Sarpy%20County%2C%20NE&roles[0]=Full%20time%20employment"
     },
     {
-        name: "Amazon",
+        name: "Amazon New Grad",
         url: "https://www.amazon.jobs/content/en/career-programs/university/jobs-for-grads?country%5B%5D=US"
 
     }
